8770f0bb17a29c534fd5e2e3b827a1af4714a926
danRolon <daniel.rolon@yahoo.com>
2019-06-13 14:23:02 -0500

    created Instructor class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -8,3 +8,18 @@ class Person{
     console.log(`Hello my name is ${this.name}, I am from ${this.location}.`);
   }
 }
+
+class Instructor extends Person{
+  constructor(prop){
+    this.specialty=prop.specialty;
+    this.favLanguage=prop.favLanguage;
+    this.catchPhrase=prop.catchPhrase;
+  }
+  demo(sbj){
+    console.log(`Today we are learning about ${sbj}`);
+  }
+  grade(obj,sbj){
+    console.log(`${obj.name} receives a perfect score on ${sbj}`);
+  }
+}
+
